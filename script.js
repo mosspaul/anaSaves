e9d2456ea954d34d5e196265144ad76fb8cbcf42
mosspaul <gmosspaul18@gmail.com>
2022-12-20 12:33:04 -0600

    Build goal functionality

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function getPosition(e) {
     }
 }
 
+function checkGoal(rect, posObj) {
+    h = rect.height + 100;
+    w = rect.width + 100;
+    let goal = false;
+
+    if ((w * 0.375) <= posObj.x && posObj.x <= (w * 0.675)) {
+        if ((h * 0.175) <= posObj.y && posObj.y <= (h * 0.525)) {
+            goal = true;
+        }
+    }
+    return goal;
+}
+
 GAME_AREA.addEventListener("click", pos => {
-    console.table(getPosition(pos));
+    let rect = GAME_AREA.getBoundingClientRect();
+    console.log(checkGoal(rect, getPosition(pos)));
+
 });
+
+/* 
+    width: 3/8 - [2/8] - 3/8
+    height: 240 --- 7/40 - [10/40] - 23/40
+*/
